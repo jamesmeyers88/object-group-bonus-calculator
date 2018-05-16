96202960a25c448dcf8dfbfc14a0c48a52f74c93
Sean <sean@Grace-Kasaharas-MacBook-Pro.local>
2018-05-16 10:48:49 -0500

    finished all of the requirements including the stretch goals

--- client.js
+++ client.js
@@ -17,4 +17,63 @@ class Employee{
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
+$(document).ready(function() {
+  $('#buttonGo').on( 'click', function() {
+    for (let i = 0; i < newEmployees.length; i++) {
+    $('#listDiv').append( '<h2>'  + newEmployees[i].name + '</h2>' );
+    $('#listDiv').append( '<li>Bonus Percent: ' + newEmployees[i].bonusPercentage*100 + '%</li>' );
+    $('#listDiv').append( '<li>Total Compensation: ' + newEmployees[i].totalCompensation + '</li>' );
+    $('#listDiv').append( '<li>Total Bonus: ' + newEmployees[i].totalBonus + '</li>' );
+    }
+    $('#buttonGo').remove();
+  });
+});
+
 console.log( employees );
+
+let newEmployees = [];
+
+function bonusCalc( employee ){
+    let bonusAmount = 0;
+
+    if ( employee.reviewRating <= 2 ) {
+    return 0;
+    }
+    else if ( employee.reviewRating == 3 ) {
+    bonusAmount= 0.04;
+    }
+    else if ( employee.reviewRating == 4 ) {
+    bonusAmount= 0.06;
+    }
+    else {
+    bonusAmount= 0.1;
+    }
+
+    if ( employee.employeeNumber.length == 4 ) {
+    bonusAmount+= 0.05;
+    }
+
+    if ( employee.annualSalary > 65000 ) {
+      bonusAmount -= 0.01;
+    }
+
+    if ( bonusAmount > 0.13) {
+      bonusAmount = 0.13;
+    }
+
+    return bonusAmount;
+
+  }
+
+  for ( i=0; i < employees.length; i++ ) {
+    let myEmployee = {
+      name: employees[i].name,
+      bonusPercentage : bonusCalc( employees[i] ),
+      totalCompensation: parseInt( employees[i].annualSalary ) + employees[i].annualSalary*bonusCalc( employees[i] ),
+      totalBonus: employees[i].annualSalary*bonusCalc(employees[i])
+    };
+    newEmployees.push(myEmployee);
+    console.log( myEmployee );
+  }
+
+console.log(newEmployees);
